9f910a950f1057abbd38a0d4329ac0faa29ab4fb
prithusrivastava <157226885+prithusrivastava@users.noreply.github.com>
2024-01-20 20:35:21 +0530

    Update mod.sol

--- mod.sol
+++ mod.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract DataManagement {
+    address public admin;
+    uint256 public data;
+
+    event DataUpdated(uint256 newData);
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not the admin");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+        data = 1000;
+    }
+
+    function updateData(uint256 newData, address targetAddress) external onlyAdmin {
+        require(newData > data, "New data must be greater than the current data");
+        assert(admin == targetAddress);
+        require(newData % 10 == 0, "Data must be a multiple of 10");
+        data = newData;
+        emit DataUpdated(newData);
+    }
+
+    function revertIfDataExceedsLimit(uint256 newData) internal pure {
+        if (newData > 10000) {
+            revert("Data exceeds the limit");
+        }
+    }
+}
